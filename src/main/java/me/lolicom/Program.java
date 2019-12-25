3213ef8d64b62d5f39ae8cd5c92b105f6092f84a
Leisureee <534619360@qq.com>
2019-12-25 18:33:59 +0800

    update

---
+++ src/main/java/me/lolicom/Program.java
@@ -0,0 +1,20 @@
+package me.lolicom;
+
+
+import me.lolicom.demo.Application;
+
+/**
+ * @author lolicom
+ */
+public class Program {
+    
+    /**
+     * 入口
+     *
+     * @param args 参数
+     */
+    public static void main(String[] args) {
+        Application.run(args);
+    }
+    
+}
